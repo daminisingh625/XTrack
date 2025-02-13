07e6d362768ae478d4b703f7fee2ffa75b0e0816
Damini Singh <daminisingh2531@gmail.com>
2025-02-13 08:59:58 +0530

    New component added

---
+++ frontend/src/components/Signup.jsx
@@ -0,0 +1,12 @@
+export default function Signup(){
+    return (
+        <>
+        <div className="signup form" >
+                <form action="">
+                    <input type="text" name="name"/><input type="email" name="email" />
+                    <input type="password" name="password"/>
+                </form>
+        </div>
+        </>
+    )
+}

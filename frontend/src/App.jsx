aa04ebde67449d2bb456f59aa8e6a460d50d186a
Damini Singh <daminisingh2531@gmail.com>
2025-02-19 11:15:31 +0530

    New files made

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,15 @@ import {Routes, Route} from 'react-router-dom';
 import Signup from "./components/Signup";
 import Login from "./components/Login"
 import "./App.css";
+import Dashboard from "./components/Dashboard";
 
 function App() {
   return (
     <>
       <div>
         <Routes>
-          <Route path="/" element={<Signup />} />
+          <Route path="/" element={<Dashboard/>}>
+          <Route path="/signup" element={<Signup />} />
           <Route path="/login" element={<Login/>}/>
         </Routes>
       </div>
---
+++ frontend/src/components/Dashboard.jsx
@@ -0,0 +1,8 @@
+export default function Dashboard(){
+    return (
+    <>
+    <h1>This is dashhy</h1>
+    <button>Add Expense</button>
+    </>
+    )
+}

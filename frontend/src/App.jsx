af997a60d578a68c969164a56f60c3dc51961423
Disha Navreti <dishanavreti09@gmail.com>
2025-02-20 17:59:14 +0530

    Updated sidebar and CSS styles

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,25 +5,32 @@ import Login from "./components/Login"
 import "./App.css";
 import Dashboard from "./components/Dashboard";
 import Navbar from "./components/Navbar"
+import Sidebar from "./components/Sidebar";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
-    <>
+
       <div>
         <header>
         <Navbar/>
         </header>
+        
+        <Sidebar/>
+           
+           <div className="page-content">
         <Routes>
           <Route path="/" element={<Dashboard/>}/>
           <Route path="/signup" element={<Signup />} />
           <Route path="/signin" element={<Login/>}/>
         </Routes>
+      
+        
         <footer>
-
         </footer>
       </div>
     
-    </>
+   </div>
   );
 }
 

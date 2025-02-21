9070c941fadda5d8aa00b9ae2b90e3419a7839a3
Disha Navreti <dishanavreti09@gmail.com>
2025-02-21 19:55:17 +0530

    update on sidebar

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -18,14 +18,42 @@ export default function Sidebar() {
                 </a>
             </li>
                       
-                <li><a href='#'><FaMoneyBillWave size={20} />Add Expance</a></li>
+                <li>
+                    <a href='#'><FaMoneyBillWave size={20} />
+                    Add Expance
+                    </a>
+                </li>
+                <li>
+                    <a href="#"><MdCommentBank size={20}/>
+                    Bank Details
+                    </a>
+                </li> 
+                <li>
+                   <a href="#"><TbReportAnalytics size={20}/>
+                    Reports & Analytics
+                   </a>
+                </li>
+                <li>
+                   <a href="#"><FaCalendarAlt size={20}/>
+                   Budget Planner
+                   </a>
+                </li>
+                <li>
+                    <a href="#"><FaReceipt size={20}/>
+                    Bill Reminders
+                    </a>
+                </li>
+                <li>
+                    <a href="#"><MdCategory size={20}/>
+                    Categories
+                    </a>
+                </li>
+                <li>
+                    <a href="#"><TiArrowLoopOutline size={20}/>
+                    Recurring Expenses
+                    </a>
+                </li>
 
-                <li><a href="#"><MdCommentBank size={20}/>Bank Details</a></li> 
-               <li><a href="#"><TbReportAnalytics size={20}/>Reports & Analytics</a></li>
-               <li><a href="#"><FaCalendarAlt size={20}/>Budget Planner</a></li>
-               <li><a href="#"><FaReceipt size={20}/>Bill Reminders</a></li>
-                <li><a href="#"><MdCategory size={20}/>Categories</a></li>
-                <li><a href="#"><TiArrowLoopOutline size={20}/>Recurring Expenses</a></li>
                 </ul>
             </div>
         </div>
